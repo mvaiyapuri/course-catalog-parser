8b4775887d8f6174294e701f66cd3fc585e537a0
mvaiyapuri <36544090+mvaiyapuri@users.noreply.github.com>
2023-09-04 11:25:00 -0700

    Add files via upload

---
+++ antlr-generated/CatalogLexer.py
@@ -0,0 +1,102 @@
+# Generated from Catalog.g4 by ANTLR 4.9.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\21")
+        buf.write("n\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16")
+        buf.write("\t\16\4\17\t\17\4\20\t\20\3\2\3\2\3\2\3\3\3\3\3\3\3\3")
+        buf.write("\3\3\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3\4\3\5\3\5\3\5\3\5\3")
+        buf.write("\5\3\5\3\6\3\6\3\6\3\6\3\6\3\7\3\7\3\7\3\7\3\7\3\7\3\7")
+        buf.write("\3\b\6\bE\n\b\r\b\16\bF\3\t\6\tJ\n\t\r\t\16\tK\3\n\3\n")
+        buf.write("\3\13\6\13Q\n\13\r\13\16\13R\3\f\3\f\3\r\3\r\3\16\3\16")
+        buf.write("\3\17\3\17\3\17\3\17\6\17_\n\17\r\17\16\17`\3\17\3\17")
+        buf.write("\3\20\3\20\3\20\7\20h\n\20\f\20\16\20k\13\20\3\20\3\20")
+        buf.write("\2\2\21\3\3\5\4\7\5\t\6\13\7\r\b\17\t\21\n\23\13\25\f")
+        buf.write("\27\r\31\16\33\17\35\20\37\21\3\2\6\3\2\62;\5\2\62;C\\")
+        buf.write("c|\5\2\13\f\17\17\"\"\5\2##.\60AA\2u\2\3\3\2\2\2\2\5\3")
+        buf.write("\2\2\2\2\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2")
+        buf.write("\2\2\17\3\2\2\2\2\21\3\2\2\2\2\23\3\2\2\2\2\25\3\2\2\2")
+        buf.write("\2\27\3\2\2\2\2\31\3\2\2\2\2\33\3\2\2\2\2\35\3\2\2\2\2")
+        buf.write("\37\3\2\2\2\3!\3\2\2\2\5$\3\2\2\2\7+\3\2\2\2\t\61\3\2")
+        buf.write("\2\2\13\67\3\2\2\2\r<\3\2\2\2\17D\3\2\2\2\21I\3\2\2\2")
+        buf.write("\23M\3\2\2\2\25P\3\2\2\2\27T\3\2\2\2\31V\3\2\2\2\33X\3")
+        buf.write("\2\2\2\35Z\3\2\2\2\37d\3\2\2\2!\"\7<\2\2\"#\7\"\2\2#\4")
+        buf.write("\3\2\2\2$%\7E\2\2%&\7q\2\2&\'\7w\2\2\'(\7t\2\2()\7u\2")
+        buf.write("\2)*\7g\2\2*\6\3\2\2\2+,\7V\2\2,-\7k\2\2-.\7v\2\2./\7")
+        buf.write("n\2\2/\60\7g\2\2\60\b\3\2\2\2\61\62\7W\2\2\62\63\7p\2")
+        buf.write("\2\63\64\7k\2\2\64\65\7v\2\2\65\66\7u\2\2\66\n\3\2\2\2")
+        buf.write("\678\7F\2\289\7g\2\29:\7u\2\2:;\7e\2\2;\f\3\2\2\2<=\7")
+        buf.write("R\2\2=>\7t\2\2>?\7g\2\2?@\7t\2\2@A\7g\2\2AB\7s\2\2B\16")
+        buf.write("\3\2\2\2CE\t\2\2\2DC\3\2\2\2EF\3\2\2\2FD\3\2\2\2FG\3\2")
+        buf.write("\2\2G\20\3\2\2\2HJ\t\3\2\2IH\3\2\2\2JK\3\2\2\2KI\3\2\2")
+        buf.write("\2KL\3\2\2\2L\22\3\2\2\2MN\t\4\2\2N\24\3\2\2\2OQ\5\23")
+        buf.write("\n\2PO\3\2\2\2QR\3\2\2\2RP\3\2\2\2RS\3\2\2\2S\26\3\2\2")
+        buf.write("\2TU\t\5\2\2U\30\3\2\2\2VW\5\17\b\2W\32\3\2\2\2XY\5\21")
+        buf.write("\t\2Y\34\3\2\2\2Z^\7$\2\2[_\5\21\t\2\\_\5\23\n\2]_\5\27")
+        buf.write("\f\2^[\3\2\2\2^\\\3\2\2\2^]\3\2\2\2_`\3\2\2\2`^\3\2\2")
+        buf.write("\2`a\3\2\2\2ab\3\2\2\2bc\7$\2\2c\36\3\2\2\2di\7)\2\2e")
+        buf.write("h\5\21\t\2fh\5\23\n\2ge\3\2\2\2gf\3\2\2\2hk\3\2\2\2ig")
+        buf.write("\3\2\2\2ij\3\2\2\2jl\3\2\2\2ki\3\2\2\2lm\7)\2\2m \3\2")
+        buf.write("\2\2\n\2FKR^`gi\2")
+        return buf.getvalue()
+
+
+class CatalogLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    COURSELITERAL = 2
+    TITLELITERAL = 3
+    UNITSLITERAL = 4
+    DESCLITERAL = 5
+    PREREQLITERAL = 6
+    NUMBER = 7
+    WORD = 8
+    WS = 9
+    WSPLUS = 10
+    PUNC = 11
+    UNITS = 12
+    COURSENAME = 13
+    TITLEDESC = 14
+    PREREQ = 15
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "': '", "'Course'", "'Title'", "'Units'", "'Desc'", "'Prereq'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "COURSELITERAL", "TITLELITERAL", "UNITSLITERAL", "DESCLITERAL", 
+            "PREREQLITERAL", "NUMBER", "WORD", "WS", "WSPLUS", "PUNC", "UNITS", 
+            "COURSENAME", "TITLEDESC", "PREREQ" ]
+
+    ruleNames = [ "T__0", "COURSELITERAL", "TITLELITERAL", "UNITSLITERAL", 
+                  "DESCLITERAL", "PREREQLITERAL", "NUMBER", "WORD", "WS", 
+                  "WSPLUS", "PUNC", "UNITS", "COURSENAME", "TITLEDESC", 
+                  "PREREQ" ]
+
+    grammarFileName = "Catalog.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
